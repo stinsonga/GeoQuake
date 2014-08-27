9400b75867fdd3c2f32087bdf972a81b51a4b50a
George Stinson <stinsonga@gmail.com>
2014-08-26 21:17:55 -0400

    Added layout for eventual side menu. Also added ListView element to quake list activity.

--- src/com/geo/GeoQuake/ListQuakes.java
+++ src/com/geo/GeoQuake/ListQuakes.java
@@ -2,14 +2,21 @@ package com.geo.GeoQuake;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 /**
  * Created by gaius on 2014-08-25.
  */
 public class ListQuakes extends Activity {
+
+    ListView quakeListView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_quakes_layout);
+
+        quakeListView = (ListView) findViewById(R.id.quakeListView);
+
     }
 }
