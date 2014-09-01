dafe7ea1597de9984dcb18daf19b4dd150fc5973
George Stinson <stinsonga@gmail.com>
2014-08-31 23:42:05 -0400

    Added shell of eventual settings activity

---
+++ src/com/geo/GeoQuake/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.geo.GeoQuake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gaius on 2014-08-31.
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity_layout);
+    }
+}
