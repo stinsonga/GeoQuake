01adfc06a0f06345c5262a435210762de7b2ee53
George Stinson <stinsonga@gmail.com>
2015-03-03 23:58:52 -0500

    WIP for list item view and adapter
    - Ok, to heck with the tabs, I don't want 'em.

---
+++ src/com/geo/GeoQuake/QuakeListAdapter.java
@@ -0,0 +1,54 @@
+package com.geo.GeoQuake;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by gaius on 15-03-03.
+ */
+public class QuakeListAdapter extends ArrayAdapter<Feature> {
+
+    private Context mContext;
+    ArrayList<Feature> mFeature;
+
+    public QuakeListAdapter(Context context, ArrayList<Feature> features){
+        super(context, R.layout.quake_list_item_layout);
+        this.mContext = context;
+        this.mFeature = features;
+    }
+
+    static class ViewHolder {
+        TextView magnitudeTextView;
+        TextView locationTextView;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            view = inflater.inflate(R.layout.quake_list_item_layout, null);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.magnitudeTextView = (TextView) view.findViewById(R.id.list_magnitude_text);
+            viewHolder.locationTextView = (TextView) view.findViewById(R.id.list_location_text);
+            view.setTag(viewHolder);
+        }
+
+        Feature feature = mFeature.get(position);
+        if(feature != null){
+            ViewHolder vh = (ViewHolder) view.getTag();
+            vh.magnitudeTextView.setText(feature.getProperties().getPlace());
+            vh.locationTextView.setText(""+feature.getProperties().getMag());
+        }
+
+        return view;
+    }
+
+}
