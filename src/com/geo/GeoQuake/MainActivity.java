aeb389df43ec7fa12fd5534bb463fe5b178c7426
George Stinson <stinsonga@gmail.com>
2016-02-25 21:29:06 -0500

    Moving some member vars over, along with menu options

--- src/com/geo/GeoQuake/MainActivity.java
+++ src/com/geo/GeoQuake/MainActivity.java
@@ -1,10 +1,36 @@
 package com.geo.GeoQuake;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    SharedPreferences mSharedPreferences;
+    SharedPreferences.Editor mSharedPreferencesEditor;
+    Context mContext;
+    Bundle mBundle;
+
+    LinearLayout mDrawerLinearLayout;
+    DrawerLayout mDrawerLayout;
+    Spinner mQuakeTypeSpinner;
+    Spinner mDurationTypeSpinner;
+    Spinner mCacheTimeSpinner;
+    CheckBox mActionBarCheckbox;
+    CheckBox mWifiCheckbox;
+    boolean mRefreshList = true;
+    boolean mAsyncUnderway = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +61,44 @@ public class MainActivity extends Activity {
         return super.onMenuItemSelected(featureId, item);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_map_view:
+                Intent intent = new Intent(this, MapActivity.class);
+//                intent.putExtra("quake_strength", mStrengthSelection);
+//                intent.putExtra("quake_duration", mDurationSelection);
+                startActivity(intent);
+                break;
+            case R.id.action_refresh:
+                mDrawerLayout.closeDrawers();
+                if (!mAsyncUnderway) {
+                    if (GeoQuakeDB.checkRefreshLimit(GeoQuakeDB.getTime(),
+                            mSharedPreferences.getLong(Utils.REFRESH_LIMITER, 0))) {
+                        SharedPreferences.Editor editor = mSharedPreferences.edit();
+                        editor.putLong(Utils.REFRESH_LIMITER, GeoQuakeDB.getTime());
+                        editor.apply();
+                        mRefreshList = true;
+                        //networkCheckFetchData();
+                    } else {
+                        Toast.makeText(mContext, getResources().getString(R.string.refresh_warning), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(mContext, getResources().getString(R.string.wait_for_loading), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.action_settings:
+                if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    mDrawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                }
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
 
 }
