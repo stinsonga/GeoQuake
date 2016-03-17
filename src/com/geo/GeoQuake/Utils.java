6bedeb8443cf8b29667b009c1700d4e66e2d3793
George Stinson <stinsonga@gmail.com>
2016-03-16 22:53:05 -0400

    Restoring 30-day logic, but not UI elements for now

--- src/com/geo/GeoQuake/Utils.java
+++ src/com/geo/GeoQuake/Utils.java
@@ -159,24 +159,20 @@ public class Utils {
                 return context.getString(R.string.all_week);
             }
         }
-
-        /*
-        Removed the past month option, due to OOM issues. Code is left here, for a future optimization update when it may be enabled again.
-
-         */
-//        else if(durationSelection == 3){
-//            if(quakeSelection == 0){
-//                return mContext.getString(R.string.significant_month);
-//            }else if(quakeSelection == 1){
-//                return mContext.getString(R.string._4_5_month);
-//            }else if(quakeSelection == 2){
-//                return mContext.getString(R.string._2_5_month);
-//            }else if(quakeSelection == 3){
-//                return mContext.getString(R.string._1_0_month);
-//            }else if(quakeSelection == 4){
-//                return mContext.getString(R.string.all_month);
-//            }
-//        }
+        //Unreachable with current UI options... may return soon
+        else if(durationSelection == 3){
+            if(quakeSelection == 0){
+                return context.getString(R.string.significant_month);
+            }else if(quakeSelection == 1){
+                return context.getString(R.string._4_5_month);
+            }else if(quakeSelection == 2){
+                return context.getString(R.string._2_5_month);
+            }else if(quakeSelection == 3){
+                return context.getString(R.string._1_0_month);
+            }else if(quakeSelection == 4){
+                return context.getString(R.string.all_month);
+            }
+        }
         return context.getString(R.string.significant_week);
 
     }
