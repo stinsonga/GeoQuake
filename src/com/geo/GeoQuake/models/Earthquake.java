ef07fc53e592801caa1f4abb2cb0c3232c624e3a
George Stinson <stinsonga@gmail.com>
2016-10-25 23:08:31 -0400

    Good enough for now, for an Earthquake model

--- src/com/geo/GeoQuake/models/Earthquake.java
+++ src/com/geo/GeoQuake/models/Earthquake.java
@@ -5,4 +5,44 @@ package com.geo.GeoQuake.models;
  */
 
 public class Earthquake {
+
+    protected String place;
+    protected String url;
+    protected double mag;
+    protected double latitude;
+    protected double longitude;
+    protected long time;
+
+    public Earthquake(double latitude, double longitude, double mag, String place, long time, String url) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.mag = mag;
+        this.place = place;
+        this.time = time;
+        this.url = url;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getMag() {
+        return mag;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
